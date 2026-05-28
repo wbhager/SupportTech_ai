d669ae375718394e55f53fe794d25fa1d15e5fbd
Will Hager <wbhager@gmail.com>
2026-05-27 23:45:36 -0400

    constructed other handlers to handle the other filetypes and used if instead of file types for standard naming convention

--- backend/tools/file_reader.py
+++ backend/tools/file_reader.py
@@ -4,27 +4,23 @@ import os
 # pdf_locator_path = /Users/willhager/Downloads
 
 def handle_pdf(file_path):
-    with pdfplumber.open(file_path) as pdf:
-        full_text = "\n\n".join(page.extract_text() for page in pdf.pages)
+    with pdfplumber.open(file_path) as f:
+        full_text = "\n\n".join(page.extract_text() for page in f.pages)
     return full_text
 
-def handle_txt(file_path):
-    return None
-
-def handle_md(file_path):
-    return None
-
-def handle_py(file_path):
-    return None
+def handle_plaintext(file_path):
+    with open(file_path, "r") as f:
+        return f.read()
 
 def handle_json(file_path):
-    return None
+    with open(file_path, "r") as f:
+        return json.dumps(json.load(f), indent=2)
 
 handlers = {
     ".pdf": handle_pdf,
-    ".txt": handle_txt,
-    ".md": handle_md,
-    ".py": handle_py,
+    ".txt": handle_plaintext,
+    ".md": handle_plaintext,
+    ".py": handle_plaintext,
     ".json": handle_json,
 }
 
