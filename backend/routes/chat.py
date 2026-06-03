3e18b065bb254468ec88241b0b427c8d7a710004
Will Hager <wbhager@gmail.com>
2026-06-03 14:24:35 -0400

    added parse_log tool for the chat endpoint so qwen knows it can utilize this tool

--- backend/routes/chat.py
+++ backend/routes/chat.py
@@ -4,6 +4,7 @@ from backend.logic.orchestrator import orchestrate
 from backend.logic.responder import respond_to_user
 from backend.tools.web_search import web_search
 from backend.tools.file_reader import file_reader
+from backend.tools.log_parser import parse_python_traceback
 import json
 
 router = APIRouter()
@@ -29,6 +30,11 @@ async def chat_endpoint(message: Message):
         file_path = json.loads(tool_args)["file_path"]
         query = json.loads(tool_args)["query"]
         tool_result = file_reader(file_path, query)
+
+    if tool_name == "parse_log":
+        text = json.loads(tool_args)["text"]
+        query = json.loads(tool_args)["query"]
+        tool_result = file_reader(text, query)
 
     response = respond_to_user(message.message, tool_name=tool_name, tool_result=tool_result)
     return {"response": response, "searching": searching}
