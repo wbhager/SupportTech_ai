21d48643a68d2eb879d7406326f4be16bf1fb9f9
Will Hager <wbhager@gmail.com>
2026-09-16 13:31:27 -0400

    added admin page to main, allowed for cursor to pop up on admin page but still keep styled cursor for main page

--- frontend-react/src/main.tsx
+++ frontend-react/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App_new.tsx'
+import AdminPage from './AdminPage.tsx'
+
+const isAdmin = window.location.pathname === '/admin'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {isAdmin ? <AdminPage /> : <App />}
   </StrictMode>,
 )
